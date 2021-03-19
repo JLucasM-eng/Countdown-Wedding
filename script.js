b33e2bb75d3bf9e224875be3fc611dbf4afcbd43
JLucasM-eng <77862386+JLucasM-eng@users.noreply.github.com>
2021-03-19 09:09:08 -0300

    Add logica. v1

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 const elemento = document.querySelector(".container")
+
+const dayspacial = new Date('Jun 5, 2021 00:00:00').getTime()
+
+function wedding(){
+    let today = new Date().getTime()
+    let falta = dayspacial - today
+    
+    const segundos = 1000;
+    const minutos = segundos*60;
+    const horas = minutos*60;
+    const dias = horas*24;
+
+    const day = Math.floor(falta/dias)
+    const hours = (Math.floor((falta%dias)/horas))
+    const minutes = (Math.floor((falta%horas)/minutos))
+    const seconds = (Math.floor((falta%minutos)/segundos)) 
+
+    document.getElementById("day").innerText = day
+    document.getElementById("hour").innerText = hours
+    document.getElementById("minute").innerText = minutes
+    document.getElementById("second").innerText = seconds
+
+
+}
+
+setInterval(()=>{
+    wedding()
+},1000)
